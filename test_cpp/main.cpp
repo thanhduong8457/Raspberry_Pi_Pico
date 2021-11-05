6744a33f3d3f8d8815b0c55e94fbcec39e5de8a3
Le Thanh Duong <duong.le-thanh@banvien.com.vn>
2021-11-05 09:44:44 +0700

    add test

--- test_cpp/main.cpp
+++ test_cpp/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+typedef struct my_data
+{
+	int red;
+	int green;
+}my_data_t;
+
+std::vector<my_data_t *> m_my_data;
+
+void create_data(int a, int b)
+{
+	my_data_t *data = NULL;
+	data = new my_data_t;
+	data->red = a;
+	data->green = b;
+	m_my_data.push_back(data);
+}
 
 int main(void)
 {
-	cout<<"hello world, this is a test"<<endl;
-	
+
+	cout<<"Hola"<<endl;
+
 	return 0;
 }
